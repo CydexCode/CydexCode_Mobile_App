e1264b7b466d7d31988e18e0c714a165739beec4
Sachinthana Buddhika <112784979+CydexCode@users.noreply.github.com>
2024-07-06 09:20:36 +0530

    searchBar_WelcomeHeader

--- app/Pages/Home.js
+++ app/Pages/Home.js
@@ -2,52 +2,45 @@ import React from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { useUser, useClerk } from '@clerk/clerk-expo';
 import WelcomeHeader from '../Components/WelcomeHeader'
+import SearchBar from '../Components/SerchBar';
 
 export default function Home() {
-  const { signOut } = useClerk();
-  
-  return (
-    <View>
-      
-      <WelcomeHeader/>
-      
-    </View>
-  );
+    const { signOut } = useClerk();
+
+    return (
+        <View style={{ padding: 20 }}>
+
+            <WelcomeHeader />
+            <SearchBar />
+
+            <TouchableOpacity style={styles.button} onPress={() => signOut()}>
+                <Text style={styles.buttonText}>Log Out</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: 40,
-    marginTop: -20,
-    backgroundColor: '#fff',
-    borderTopRightRadius: 20,
-    borderTopLeftRadius: 20,
-    paddingBottom: 150,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  WelcomeText: {
-    fontSize: 30,
-    textAlign: 'center',
-    fontWeight: 'bold',
-  },
-  button: {
-    backgroundColor: '#32CD32',
-    padding: 10,
-    margin: 30,
-    borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-  },
+
+
+    button: {
+        marginTop: 600,
+        backgroundColor: '#32CD32',
+        padding: 10,
+        margin: 30,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,
+    },
 });
 
 
 
 
 //<TouchableOpacity style={styles.button} onPress={() => signOut()}>
-//<Text style={styles.buttonText}>Log Out</Text> 
+//<Text style={styles.buttonText}>Log Out</Text>
 //</TouchableOpacity>
